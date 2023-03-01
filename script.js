468c96d02c4bcc2b70981ccf067f682608cf107d
nuneszk <110551255+nuneszk@users.noreply.github.com>
2023-03-01 10:24:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction(){
+   var nome = document.getElementById("responder").value;
+var resposta="seja bem vindo:";
+
+document.getElementsByName("text1")[0].value = resposta + nome;
+}
